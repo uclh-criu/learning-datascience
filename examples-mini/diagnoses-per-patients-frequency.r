a54c44108694297312a09d8cc475c8044d9c8195
AndySouth <southandy@gmail.com>
2021-12-23 23:28:22 +0000

    per patient counts

---
+++ examples-mini/diagnoses-per-patients-frequency.r
@@ -0,0 +1,30 @@
+# diagnoses-per-patients-frequency.r
+# andy south 2021-12-23
+
+# summarise the frequency of unique diagnoses across patients
+
+library(tidyverse) #for packages dplyr
+
+# create a small example dataframe with just one column
+df1 <- data_frame( patient = c(1,1,1,2,2,2,3,3,3,4,5,5),
+                   diagnosis = c('a','b','c','d','d','e','f','f','f','g','h','i'))
+
+# count diagnoses per patient using dplyr
+# key part is 'group_by()'
+df2 <- df1 %>% 
+  group_by(patient) %>% 
+  summarize(n_diagnoses = length(unique(diagnosis)))
+
+# proportion of patients with each number of diagnoses
+# count counts the number of patients with each number of diagnoses (& names it n_patients)
+# mutate & prop.table create a new column containing the proportions
+df3 <- df2 %>% 
+  count(n_diagnoses, name='n_patients') %>% 
+  mutate(propn=prop.table(n_patients))
+
+df3
+
+# n_diagnoses n_patients propn
+# 1           2          0.4
+# 2           2          0.4
+# 3           1          0.2
---
+++ examples-mini/diagnosis-per-patient-count.r
@@ -0,0 +1,22 @@
+# diagnosis-per-patient-count.r
+# andy south 2021-12-23
+
+# count number of unique diagnoses per patient
+
+library(tidyverse) #for packages dplyr
+
+# create a small example dataframe with just one column
+df1 <- data_frame( patient = c(1,1,1,2,2,2,3,3,3),
+                   diagnosis = c('a','b','c','d','d','e','f','f','f'))
+
+# count diagnoses per patient using dplyr
+# key part is 'group_by()'
+df2 <- df1 %>% 
+  group_by(patient) %>% 
+  summarize(n_diagnoses = length(unique(diagnosis)))
+
+df2
+#         patient n_diagnoses
+# 1       1           3
+# 2       2           2
+# 3       3           1
